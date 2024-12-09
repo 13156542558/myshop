22e6ad4b81e7570e839b611b0ade93ac8d75e8d0
FangshaoBai <1635188614@qq.com>
2024-12-10 00:25:41 +0800

    提交

--- src/main/java/ltd/newbee/mall/service/NewBeeMallGoodsService.java
+++ src/main/java/ltd/newbee/mall/service/NewBeeMallGoodsService.java
@@ -9,43 +9,36 @@ import java.util.List;
 public interface NewBeeMallGoodsService {
     /**
      * 后台分页
-     * @param pageUtil
      */
     PageResult getNewBeeMallGoodsPage(PageQueryUtil pageUtil);
 
     /**
      * 添加商品
-     * @param goods
      */
     String saveNewBeeMallGoods(NewBeeMallGoods goods);
 
     /**
      * 批量新增商品数据
-     * @param newBeeMallGoodsList
      */
     void batchSaveNewBeeMallGoods(List<NewBeeMallGoods> newBeeMallGoodsList);
 
     /**
      * 修改商品信息
-     * @param goods
      */
     String updateNewBeeMallGoods(NewBeeMallGoods goods);
 
     /**
      * 获取商品详情
-     * @param id
      */
     NewBeeMallGoods getNewBeeMallGoodsById(Long id);
 
     /**
      * 批量修改销售状态(上架下架)
-     * @param ids
      */
     Boolean batchUpdateSellStatus(Long[] ids,int sellStatus);
 
     /**
      * 商品搜索
-     * @param pageUtil
      */
     PageResult searchNewBeeMallGoods(PageQueryUtil pageUtil);
 }
